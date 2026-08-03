a31792e92e4ef480470dbe9393632af826a335a3
zahiru <zahirulopel@gmail.com>
2026-08-03 10:34:19 +0600

    Add style of login component

--- client/src/pages/auth/component/login.tsx
+++ client/src/pages/auth/component/login.tsx
@@ -1,31 +1,45 @@
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+
 export default function Login() {
   return (
-    <section className="space-y-2 px-3">
+    <section className="space-y-4 px-3">
       <h2 className="text-4xl font-bold">Welcome back</h2>
-      <p className="text-xl">New to Odin Messaging App? Create an account</p>
-      <form className="flex flex-col gap-y-2">
-        <div className="flex flex-col">
+      <p className="text-xl text-gray-800">
+        New to Odin Messaging App? Create an account
+      </p>
+      <form className="flex flex-col gap-y-3 text-gray-800">
+        <div className="flex flex-col gap-y-1">
           <label htmlFor="email">Email</label>
           <input
-            className="rounded-lg border border-gray-200 px-2 py-1 focus:outline-1 focus:outline-gray-400"
+            className="h-10 rounded-lg border border-gray-200 px-2 py-1 focus:outline-1 focus:outline-gray-400"
             type="email"
             name="email"
             id="email"
+            placeholder="Enter your email"
           />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-y-1">
           <label htmlFor="password">Password</label>
           <input
-            className="rounded-lg border border-gray-200 px-2 py-1 focus:outline-1 focus:outline-gray-400"
+            className="h-10 rounded-lg border border-gray-200 px-2 py-1 focus:outline-1 focus:outline-gray-400"
             type="password"
             name="password"
             id="password"
           />
         </div>
-        <button className="" type="submit">
+        <button
+          className="rounded-lg bg-orange-600 p-2 font-bold text-white transition-colors duration-100 hover:cursor-pointer hover:bg-orange-500 focus:bg-orange-500"
+          type="submit"
+        >
           Log in
         </button>
-        <button type="button">Try a demo account</button>
+        <button
+          className="group flex justify-center gap-1 rounded-lg border border-orange-600 p-2 transition-colors duration-100 hover:cursor-pointer hover:border-white hover:bg-orange-400 hover:text-white focus:bg-orange-400 focus:text-white"
+          type="button"
+        >
+          <UserCircleIcon className="size-6 text-orange-600 transition-colors duration-100 group-hover:text-white group-focus:text-white" />
+          <span>Try a demo account</span>
+        </button>
       </form>
     </section>
   );
