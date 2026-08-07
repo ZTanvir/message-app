5a34835bbc4ec9f442703e326398804242e290ec
zahiru <zahirulopel@gmail.com>
2026-08-07 10:50:53 +0600

    Change db user and profile schema and frontend form validation

--- client/src/utils/schemas/validators.ts
+++ client/src/utils/schemas/validators.ts
@@ -6,15 +6,25 @@ export const LoginFormSchema = z.object({
     .string()
     .trim()
     .min(8, "Password must be at least 8 characters long")
+    .max(255, "Password must be with in 255 characters")
     .regex(/\d/, "Password must contain at least one number"),
 });
 
 export const SignUpFormSchema = z.object({
-  first_name: z.string().trim().min(1, "First name is required"),
-  last_name: z.string().trim().min(1, "Last name is required"),
+  first_name: z
+    .string()
+    .trim()
+    .min(1, "First name is required")
+    .max(50, "First name must be with in 50 characters"),
+  last_name: z
+    .string()
+    .trim()
+    .min(1, "Last name is required")
+    .max(50, "Last name must be with in 50 characters"),
   email: z.email(),
   password: z
     .string()
     .min(8, "Password must be at least 8 characters long")
+    .max(255, "Password must be with in 255 characters")
     .regex(/\d/, "Password must contain at least one number"),
 });
