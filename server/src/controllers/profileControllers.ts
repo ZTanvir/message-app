a1b0c45277058ee92d016e9ac6d8d55a981ceace
zahiru <zahirulopel@gmail.com>
2026-08-25 09:44:16 +0600

    Add get profile route to get profile of an user

--- server/src/controllers/profileControllers.ts
+++ server/src/controllers/profileControllers.ts
@@ -1,17 +1,16 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { prisma } from "../lib/prisma.ts";
 import AppError from "../utils/appError.ts";
 
 export async function getProfile(req: Request, res: Response) {
   const { userId } = req.params;
-  if (!userId) throw new AppError("User id not found", 404);
+  if (!userId) throw new AppError("User id not found", 400);
 
-  const normalizeUserId = Array.isArray(userId)
-    ? (userId[0] ?? undefined)
-    : (userId ?? undefined);
+  const normalizeUserId = Array.isArray(userId) ? userId[0] : userId;
 
   const user = await prisma.profile.findUnique({
     where: { userId: normalizeUserId },
   });
+  if (!user) throw new AppError("Profile not found", 404);
   return res.status(200).json({ data: user });
 }
