57624a47dd86a6f7300b74330d27bcc779289b62
zahiru <zahirulopel@gmail.com>
2026-08-30 10:21:04 +0600

    Add uploaded image demo preview in CoverPhotoModal

--- client/src/pages/profile/components/CoverPhotoModal.tsx
+++ client/src/pages/profile/components/CoverPhotoModal.tsx
@@ -17,27 +17,41 @@ export default function CoverPhotoModal({
   const handleRemoveImage = () => {
     setImgSrc(null);
   };
+  const handleUploadImage = (
+    e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
+  ) => {
+    const file = e.currentTarget.files?.[0];
+    if (!file) return;
+    const objectUrl = URL.createObjectURL(file);
+    setImgSrc(objectUrl);
+  };
+
   return (
     <Modal ref={ref} title="Edit cover image">
       <div>
         <div className="relative mt-3 mb-3 rounded-sm border border-dotted border-gray-200 p-3">
           {imgSrc ? (
-            <img src={imageUrl} />
+            <>
+              <img
+                className="m-auto h-50 w-50 rounded-full object-cover"
+                alt="demo profile"
+                src={imgSrc}
+              />
+              <XMarkIcon
+                onClick={handleRemoveImage}
+                title="clear"
+                className="absolute top-1 right-1 h-8 w-8 rounded-full bg-gray-100 p-1 hover:cursor-pointer"
+              />
+            </>
           ) : (
             <p className="text-center">Not image added</p>
-          )}
-          {imgSrc && (
-            <XMarkIcon
-              onClick={handleRemoveImage}
-              className="absolute top-0 right-0 h-8 w-8 rounded-full bg-gray-100 p-1"
-            />
           )}
         </div>
         <div className="flex flex-col justify-between gap-y-2 md:flex-row">
           <div>
-            <button className="w-full rounded-sm border border-orange-600 font-bold hover:cursor-pointer">
+            <button className="w-full rounded-sm border border-orange-600 font-bold">
               <label
-                className="flex justify-center gap-2 px-5 py-2"
+                className="flex justify-center gap-2 px-5 py-2 opacity-80 hover:cursor-pointer hover:opacity-100"
                 htmlFor="myfile"
               >
                 <span>
@@ -46,7 +60,14 @@ export default function CoverPhotoModal({
                 Upload Image
               </label>
             </button>
-            <input className="hidden" type="file" id="myfile" name="myfile" />
+            <input
+              onChange={handleUploadImage}
+              accept="image/*"
+              className="hidden"
+              type="file"
+              id="myfile"
+              name="myfile"
+            />
           </div>
 
           <button className="rounded-sm bg-orange-600 px-5 py-2 font-bold text-white hover:cursor-pointer">
