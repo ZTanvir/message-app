986b30e8399b380981657d7c83c1c8f506699847
zahiru <zahirulopel@gmail.com>
2026-09-26 12:12:09 +0600

    Fixed Conversation user messages list height issues

--- client/src/pages/home/components/Conversation.tsx
+++ client/src/pages/home/components/Conversation.tsx
@@ -14,27 +14,30 @@ export default function Conversation() {
     refetch();
   };
   const Header = (
-    <h2 className="flex items-center gap-x-1 px-4 py-6">
-      <Bars3CenterLeftIcon className="h-7 w-7" />
-      <span className="text-2xl">User Messages</span>
-    </h2>
+    <header className="flex flex-col">
+      <h2 className="flex items-center gap-x-1 px-4 py-6">
+        <Bars3CenterLeftIcon className="h-7 w-7" />
+        <span className="text-2xl">User Messages</span>
+      </h2>
+      <hr className="text-gray-300" />
+    </header>
   );
   if (isPending) {
     return (
-      <div className="flex h-full w-1/3 flex-col border-r border-gray-300 shadow-md">
+      <div className="flex min-h-[92dvh] w-1/3 flex-col border-r border-gray-300 shadow-md lg:h-full">
         {Header}
-        <div className="flex h-full items-center justify-center">
-          <Spinner classname="w-15 h-15 border-3" />
+        <div className="flex flex-1 items-center justify-center">
+          <Spinner classname="md:w-10 md:h-10 lg:w-15 lg:h-15 border-3" />
         </div>
       </div>
     );
   }
-  if (!isError) {
+  if (isError) {
     return (
-      <div className="flex h-full w-1/3 flex-col border-r border-gray-300 shadow-md">
+      <div className="flex min-h-[92vh] w-1/3 flex-col border-r border-gray-300 shadow-md lg:h-full">
         {Header}
-        <div className="flex h-full flex-col items-center justify-center gap-y-1">
-          <p>{"error.message" || "Something went wrong"}</p>
+        <div className="flex flex-1 flex-col items-center justify-center gap-y-1">
+          <p>{error.message || "Something went wrong"}</p>
           <button
             className="rounded-md bg-orange-400 px-3 py-1 text-sm text-white transition-colors duration-300 hover:cursor-pointer hover:bg-orange-400/80"
             onClick={handleRetryFetch}
@@ -46,9 +49,11 @@ export default function Conversation() {
     );
   }
   return (
-    <div>
-      {Header}
-      <div></div>
+    <div className="flex min-h-[92dvh] lg:h-full">
+      <div className="w-1/3 border-r border-gray-300 shadow-md">
+        {Header}
+        <Link to="10">chat</Link>
+      </div>
       <Outlet />
     </div>
   );
